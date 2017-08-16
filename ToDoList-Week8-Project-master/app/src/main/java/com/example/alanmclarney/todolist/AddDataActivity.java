ebc2791b6674b4f003e6b2ece6ea7e8acae9ff7e
Alan McLarney <alanmclarney@gmail.com>
2017-08-16 14:09:40 +0100

    screens set up

---
+++ ToDoList-Week8-Project-master/app/src/main/java/com/example/alanmclarney/todolist/AddDataActivity.java
@@ -0,0 +1,18 @@
+package com.example.alanmclarney.todolist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alanmclarney on 16/08/2017.
+ */
+
+public class AddDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_data);
+
+    }
+}
--- ToDoList-Week8-Project-master/app/src/main/java/com/example/alanmclarney/todolist/MainActivity.java
+++ ToDoList-Week8-Project-master/app/src/main/java/com/example/alanmclarney/todolist/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -44,10 +45,10 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-
-
-
-
+    public void addNewItem(View v) {
+        Intent intent = new Intent(this, AddDataActivity.class);
+        startActivity(intent);
+    }
 
 
 }
